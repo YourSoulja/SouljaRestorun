f18bdb6a1217a0dbf39c4fb37cfc02b31f9ef17e
YourSoulja <157317101+YourSoulja@users.noreply.github.com>
2025-02-18 23:18:57 +0400

    fir

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/SouljaRestorun/'>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <StrictMode>
       <App />
     </StrictMode>
