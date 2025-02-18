325a01b990378409fe8ed0f5ae8fb64048c5fca0
YourSoulja <157317101+YourSoulja@users.noreply.github.com>
2025-02-18 22:49:06 +0400

    first co

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
@@ -7,6 +8,6 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter basename='/SouljaRestorun/'>
     <StrictMode>
       <App />
-    </StrictMode>,
+    </StrictMode>
   </BrowserRouter>
 )
